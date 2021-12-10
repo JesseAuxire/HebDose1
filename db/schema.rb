4c90cf0c9997dbd2565b321e18b2876b2de6e7d5
Jesse Auxire <auxire.jesse@hotmail.fr>
2021-12-10 23:37:23 +0100

    like button

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_09_112333) do
+ActiveRecord::Schema.define(version: 2021_12_10_155429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "story_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["story_id"], name: "index_likes_on_story_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -48,6 +57,8 @@ ActiveRecord::Schema.define(version: 2021_12_09_112333) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "stories"
+  add_foreign_key "likes", "users"
   add_foreign_key "reviews", "stories"
   add_foreign_key "reviews", "users"
 end
